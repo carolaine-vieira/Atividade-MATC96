fb80de0ae5791fe4b3b4ea3dcf86632a64295561
Carolaine Vieira <s.carolaine.v@gmail.com>
2023-09-20 17:08:56 -0300

    first commit

---
+++ lab.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Ler a planilha CSV
+df = pd.read_csv('doc.csv')
+
+# Extrair os textos das colunas 'Title' e 'Abstract'
+documentos = df['Title'] + ' ' + df['Abstract']
+query = input("Digite a sua query: ")
+
+# Inicializar o vetorizador TF-IDF
+tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+
+# Transformar os documentos e a query em vetores TF-IDF
+tfidf_matrix = tfidf_vectorizer.fit_transform(documentos)
+query_vector = tfidf_vectorizer.transform([query])
+
+# Calcular a similaridade cosseno entre a query e os documentos
+similaridades = cosine_similarity(query_vector, tfidf_matrix)
+
+# Criar uma lista de tuplas (título do documento, pontuação de similaridade)
+ranqueamento = [(df.iloc[i]['Title'], similaridade)
+                for i, similaridade in enumerate(similaridades[0])]
+
+# Ordenar a lista pelo valor de similaridade em ordem decrescente
+ranqueamento = sorted(ranqueamento, key=lambda x: x[1], reverse=True)
+
+document = []
+
+# Iterar sobre o array do ranqueamento pra gerar o arquivo
+for i, (titulo, score) in enumerate(ranqueamento, start=1):
+    linha = {'Pontuação': score, 'Título': titulo}
+    document.append(linha)
+
+ranking_file = pd.DataFrame(document)
+ranking_file.to_csv('ranking.csv')
